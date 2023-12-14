210eb3c8dacd29e9bfb8401aad6a55ac65b9608d
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2023-12-14 13:49:07 -0500

    Add statics

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,3 +1,8 @@
+// * Global Resources
+
+let H = window.innerHeight * 0.65;
+let W = window.innerWidth * 0.75;
+
 // * Code for Dependency Graph
 
 // Fetching Data
@@ -52,3 +57,42 @@ fetch("../d3-data/labels.json")
     .catch((error) => {
         console.error("There was a problem fetching the JSON file:", error);
     });
+
+// Creating Play Pen 1
+const svg1 = d3
+    .select("div#plot1")
+    .append("svg")
+    .attr("width", W)
+    .attr("height", H);
+
+const pen1 = svg1
+    .append("rect")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("fill", "#d3e4cd");
+
+// Creating Play Pen 2
+const svg2 = d3
+    .select("div#plot2")
+    .append("svg")
+    .attr("width", W)
+    .attr("height", H);
+
+const pen2 = svg2
+    .append("rect")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("fill", "#d3e4cd");
+
+// Creating Play Pen 3
+const svg3 = d3
+    .select("div#plot3")
+    .append("svg")
+    .attr("width", W)
+    .attr("height", H);
+
+const pen3 = svg3
+    .append("rect")
+    .attr("width", "100%")
+    .attr("height", "100%")
+    .attr("fill", "#d3e4cd");
