840ee5f5eec6ede6ab4299b7d4a51a0ccf446ae2
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2023-12-14 14:24:58 -0500

    Add levenshtein distance

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -58,6 +58,88 @@ fetch("../d3-data/labels.json")
         console.error("There was a problem fetching the JSON file:", error);
     });
 
+// General
+function levenshteinDistance(a, b) {
+    if (a.length === 0) return b.length;
+    if (b.length === 0) return a.length;
+
+    const matrix = [];
+
+    // Initialize matrix
+    for (let i = 0; i <= b.length; i++) {
+        matrix[i] = [i];
+    }
+
+    for (let j = 0; j <= a.length; j++) {
+        matrix[0][j] = j;
+    }
+
+    // Calculate Levenshtein distance
+    for (let i = 1; i <= b.length; i++) {
+        for (let j = 1; j <= a.length; j++) {
+            const cost = a.charAt(j - 1) === b.charAt(i - 1) ? 0 : 1;
+            matrix[i][j] = Math.min(
+                matrix[i - 1][j] + 1,
+                matrix[i][j - 1] + 1,
+                matrix[i - 1][j - 1] + cost
+            );
+        }
+    }
+
+    return matrix[b.length][a.length];
+}
+
+// General Update Pattern
+function bindStrong(svg, data) {
+    svg.selectAll("circle")
+        .data(data)
+        .join("circle")
+        .attr("fill", "#e6b1f8")
+        .attr("cx", (d) => d * 80)
+        .attr("cy", (d) => d * 80)
+        .attr("r", (d) => 20);
+}
+
+function bindWeak(svg, data) {
+    svg.selectAll("circle")
+        .data(data)
+        .join("circle")
+        .attr("fill", "#e6b1f8")
+        .attr("cx", (d) => d * 80)
+        .attr("cy", (d) => d * 80)
+        .attr("r", (d) => 20);
+}
+
+function drawDependencyGraph(givenWord) {
+    let closestMatch = null;
+    let minDistance = Infinity;
+    let closestIndex = null;
+
+    Object.keys(index).forEach((word) => {
+        const distance = levenshteinDistance(givenWord, word);
+        if (distance < minDistance) {
+            minDistance = distance;
+            closestMatch = word;
+            closestIndex = index[word];
+        }
+    });
+
+    console.log(closestMatch);
+    console.log(closestIndex);
+    // bindStrong(svg1);
+    // bindWeak(svg1);
+}
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+    const inputWord = document.getElementById("textbox").value.trim();
+
+    if (inputWord !== null && inputWord !== "") {
+        drawDependencyGraph(inputWord);
+    } else {
+        alert("Please enter a valid package!");
+    }
+});
+
 // Creating Play Pen 1
 const svg1 = d3
     .select("div#plot1")
@@ -70,6 +152,8 @@ const pen1 = svg1
     .attr("width", "100%")
     .attr("height", "100%")
     .attr("fill", "#d3e4cd");
+
+bindData(svg1, [1, 2, 3, 4, 5, 6]);
 
 // Creating Play Pen 2
 const svg2 = d3
